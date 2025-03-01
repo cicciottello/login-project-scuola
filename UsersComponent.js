de3089524679605c76cec71c8af299f6977acefc
cicciottello <118474551+cicciottello@users.noreply.github.com>
2025-03-01 09:40:27 +0100

    Update UsersComponent.js
    aggiunto bcrypt

--- UsersComponent.js
+++ UsersComponent.js
@@ -1,4 +1,6 @@
 const fs = require("fs")
+const bcrypt = require("bcrypt")
+const saltRounds = 10
 
 class UsersComponent {
   constructor(statePath) {
@@ -6,7 +8,7 @@ class UsersComponent {
     this.statePath = statePath
     try {
       this.users = JSON.parse(fs.readFileSync(statePath, "utf-8"))
-    } catch(err) {
+    } catch (err) {
       console.log(err.message)
       this.serialize()
     }
@@ -16,27 +18,36 @@ class UsersComponent {
     fs.writeFileSync(this.statePath, JSON.stringify(this.users, null, 2))
   }
 
-  read(){
+  read() {
     return fs.readFileSync(this.statePath)
   }
 
-  create(data) {
+  async create(data) {
     const email = data.email
-    const password = data.password
 
-    this.users[email] = {
-      email: email,
-      password: password
+    // Hash della password in modo asincrono
+    try {
+      const password = await bcrypt.hash(data.password, saltRounds)
+      
+      // Aggiungi l'utente alla lista
+      this.users[email] = {
+        email: email,
+        password: password
+      }
+
+      this.serialize()  // Salva i cambiamenti
+    } catch (err) {
+      console.log('Errore nella creazione dell\'utente:', err.message)
     }
-
-    this.serialize()
   }
 
-  login(email, password) {
+  async login(email, password) {
     const user = this.users[email]
-    
-    if (user && user.password === password) {
-      return true
+
+    if (user) {
+      // Confronta la password inserita con quella criptata
+      const match = await bcrypt.compare(password, user.password)
+      return match
     }
 
     return false
